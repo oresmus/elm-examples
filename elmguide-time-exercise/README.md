48bc7058c20e3e9c3702dd06f746f8fb00be51df
Bruce Smith <oresmus@gmail.com>
2016-10-18 18:23:47 -0700

    source for time exercise from elm guide

---
+++ elmguide-time-exercise/README.md
@@ -0,0 +1,7 @@
+The elm guide has a time example (showing a clock drawn using svg). 
+It suggests an exercise of adding a checkbox for pausing the clock.
+This requires adding a paused boolean to the model, using it to alter the subscription to time ticks,
+and showing a checkbox in the output (combining html with svg there).
+This is my code which does that exercise (by modifying the example code in the elm guide).
+
+It's mainly here to show how to draw svg, and what imports and packages are needed for that.
---
+++ elmguide-time-exercise/elmguide-time-exercise.elm
@@ -0,0 +1,83 @@
+import Html exposing (Html,div,input)
+import Html.App as App
+import Svg exposing (svg,circle,line)
+import Svg.Attributes exposing (viewBox,width,cx,cy,r,fill,x1,y1,x2,y2,stroke)
+import Time exposing (Time, second)
+
+import Html.Attributes exposing (style, type')
+import Html.Events exposing (onClick)
+
+
+
+main =
+  App.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+
+-- MODEL
+
+
+type alias Model = {time : Time, on:Bool}
+
+
+init : (Model, Cmd Msg)
+init =
+  (Model 0 True, Cmd.none)
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Tick Time | OnOff 
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Tick newTime ->
+      ({model | time = newTime}, Cmd.none)
+    OnOff -> 
+      ({model | on = not model.on}, Cmd.none)
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model = if model.on then
+  Time.every second Tick
+  else Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  let
+    angle =
+      turns (Time.inMinutes model.time)
+
+    handX =
+      toString (50 + 40 * cos angle)
+
+    handY =
+      toString (50 + 40 * sin angle)
+  in
+    div [] [
+     text "hi", text "lo",input [Html.Attributes.type' "checkbox", onClick OnOff ] [],
+     svg [ viewBox "0 0 100 100", width "300px" ]
+      [ circle [ cx "50", cy "50", r "45",  fill (if model.on then "#0B79CE" else "#7979CE") ] []
+      , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+      ]
+     ]
+
